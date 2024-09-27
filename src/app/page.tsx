db5d6b24049fd6d7abef19f678fb3453920e66e4
VunderVlad <vtitov.sk@gmail.com>
2024-09-27 07:56:43 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+export default function Home() {
+  return(
+    <Typography variant="h5" component="h6">
+      h1. Heading
+    </Typography>
+
+  );
+}
